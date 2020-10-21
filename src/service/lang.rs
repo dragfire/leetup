04980599ababf1eb355034cb941c77bba8e89f82
Devajit Asem <asem.devajit@gmail.com>
2020-10-21 17:45:40 -0600

    Add support for multiple languages (#4)
    * Add support for multiple languages
    
    * Add TODO

---
+++ src/service/lang.rs
@@ -0,0 +1,49 @@
+// TODO Add more Languages
+
+use crate::LeetUpError;
+use anyhow::anyhow;
+use std::str::FromStr;
+
+/// Store Lang attributes.
+#[derive(Debug)]
+pub struct LangInfo {
+    pub name: String,
+    pub extension: String,
+    pub comment_style: CommentStyle,
+}
+
+/// Comment styles for different languages.
+#[derive(Debug)]
+pub enum CommentStyle {
+    C(String),
+    Lisp(String),
+}
+
+/// Represent different languages supported by a Service provider.
+#[derive(Debug)]
+pub enum Lang {
+    Rust(LangInfo),
+    Java(LangInfo),
+}
+
+impl FromStr for Lang {
+    type Err = LeetUpError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let c_comment = CommentStyle::C("//".to_string());
+
+        match s {
+            "rust" => Ok(Lang::Rust(LangInfo {
+                name: "rust".to_string(),
+                extension: "rs".to_string(),
+                comment_style: c_comment,
+            })),
+            "java" => Ok(Lang::Java(LangInfo {
+                name: "java".to_string(),
+                extension: "java".to_string(),
+                comment_style: c_comment,
+            })),
+            _ => Err(LeetUpError::Any(anyhow!("Language not supported!"))),
+        }
+    }
+}
--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,5 +1,7 @@
+pub use lang::*;
 pub use provider::{Config, ServiceProvider, Session, Urls};
 
 pub mod auth;
+mod lang;
 pub mod leetcode;
 mod provider;
